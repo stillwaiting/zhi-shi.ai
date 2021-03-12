d6ae2f94936057a17ef954337339ba85c2f6c028
Boris Gvozdev <bgvozdev@gmail.com>
2021-03-12 13:45:46 +1100

    refactor SentenceComponent to avoid brainbursts

--- training-app/src/SentenceComponent.test.tsx
+++ training-app/src/SentenceComponent.test.tsx
@@ -4,7 +4,24 @@ import SentenceComponent from './SentenceComponent';
 import '@testing-library/jest-dom'
 
 test('renders without dropdowns', () => {
-  render(<SentenceComponent sentence="Hello, world!" onSubmit={() => {}} />);
-  const text = screen.getByText(/Hello, world!/i);
-  expect(text).toBeInTheDocument();
+    const component =render(<SentenceComponent sentence="Hello, world!" onSubmit={() => {}} />);
+    const text = screen.getByText(/Hello, world!/i);
+    expect(text).toBeInTheDocument();
+    expect(component.container.getElementsByTagName('select')).toHaveLength(0);
 });
+
+test('renders with dropdowns', () => {
+    const component = render(<SentenceComponent sentence="(Hello|blah|baz), (world|foo)(!|.)" onSubmit={() => {}} />);
+    const selects = component.container.getElementsByTagName('select');
+    expect(selects).toHaveLength(3);
+    const expected = [
+        ['Hello', 'blah', 'baz'],
+        ['world', 'foo'],
+        ['!', '.']
+    ];
+    expected.forEach((options, selectIdx) => {
+       options.forEach((option, optionIdx) => {
+            expect(selects[selectIdx].getElementsByTagName('option')[optionIdx].innerHTML).toEqual(option);
+       });
+    });
+  });
--- training-app/src/SentenceComponent.tsx
+++ training-app/src/SentenceComponent.tsx
@@ -1,64 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import './SentenceComponent.css';
 
+// Searches for "(blah|foo)" dropdown strings
+const DROPDOWN_REGEXP = /(\(.+?\|.*?\))/g;
+
+const parseDropdownString = (dropdownString: string) => 
+    dropdownString.substr(1, dropdownString.length - 2).split('|')
+
+const countDropdowns = (splitSentence: Array<string>) => 
+    splitSentence.filter(subSentence => subSentence.match(DROPDOWN_REGEXP)).length;
+
 type SentenceComponentProps = {
     onSubmit: (indices: Array<number>) => void, 
     sentence: string
 }
 
-const Component = ({ onSubmit, sentence }: SentenceComponentProps) => {
-  const [indices, setIndices] = useState<Array<number>>([]);
-  const [sanitizedSentence, setSanitizedSentence] = useState<Array<string>>([]);
-  const [dropdowns, setDropdowns] = useState<Array<string>>([]);
+export default ({ onSubmit, sentence }: SentenceComponentProps) => {
+    const [selectedDropdownIndices, setSelectedDropdownIndices] = useState<Array<number>>(
+        Array((sentence.match(DROPDOWN_REGEXP) || []).length).fill(0)
+    );
 
-  useEffect(() => {
-    if (sentence) {
-      setDropdowns(sentence.match(/(\(.+?\|.*?\))/g) || []);
-      setSanitizedSentence(sentence.split(/(\(.+?\|.*?\))/g) || []);
-      setIndices(Array((sentence.match(/(\(.+?\|.*?\))/g) || []).length).fill(0));
-    }
-  }, [sentence]);
+    // Each item is either an arbitrary string or a "dropdown" string "(blah|foo)"
+    const splitSentence = sentence.split(DROPDOWN_REGEXP) || [];
 
-  return (
-    <div className="SentenceComponent">
-      <div>
-        {sanitizedSentence.map((subSentence, index) => {
-          if (/(\(.+?\|.*?\))$/.test(subSentence)) {
-            const options = subSentence
-              .replace(/(\()/g, '')
-              .replace(/(\))/g, '')
-              .split('|');
-            return (
-              <span key={index}>
-                <select
-                  className="select"
-                  onChange={e => {
-                    const newIndices = [...indices];
-                    newIndices[dropdowns.indexOf(subSentence)] = Number(
-                      e.currentTarget.value
-                    );
-                    setIndices(newIndices);
-                  }}
-                >
-                  {options.map((option, idx) => {
-                    if (option)
-                      return (
-                        <option key={idx} value={idx}>
-                          {option}
-                        </option>
-                      );
-                  })}
-                </select>
-              </span>
-            );
-          } else return <span key={index}>{subSentence}</span>;
-        })}
-      </div>
-      <button className="button" onClick={() => onSubmit(indices)}>
-        Submit
-      </button>
-    </div>
-  );
+    return (
+        <div className="SentenceComponent">
+            <div>
+                {splitSentence.map((subSentence, subSentenceIdx) => {
+                    if (DROPDOWN_REGEXP.test(subSentence)) {
+                        const options = parseDropdownString(subSentence);
+                        return (
+                            <span key={subSentenceIdx}>
+                                <select
+                                    className="select"
+                                    onChange={e => {
+                                        const newIndices = [...selectedDropdownIndices];
+                                        newIndices[countDropdowns(splitSentence.slice(0, subSentenceIdx))] = parseInt(e.currentTarget.value);
+                                        setSelectedDropdownIndices(newIndices);
+                                    }}
+                                >
+                                {options.map((option, idx) => {
+                                    if (option)
+                                    return (
+                                        <option key={idx} value={idx}>
+                                        {option}
+                                        </option>
+                                    );
+                                })}
+                                </select>
+                            </span>
+                        );
+                    } else {
+                        return <span key={subSentenceIdx}>{subSentence}</span>;
+                    }
+                })}
+            </div>
+            <button className="button" onClick={() => onSubmit(selectedDropdownIndices)}>
+                Submit
+            </button>
+        </div>
+    );
 };
-
-export default Component;
