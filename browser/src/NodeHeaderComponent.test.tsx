8a803c0cb9a055934c065531073d6262fdde3300
Boris Gvozdev <bgvozdev@gmail.com>
2021-07-05 13:32:57 +1000

    fix NodeHeaderComponent tests

--- browser/src/NodeHeaderComponent.test.tsx
+++ browser/src/NodeHeaderComponent.test.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { fireEvent, render, RenderResult, screen } from '@testing-library/react';
 import TopicsTreeComponent from './TopicsTreeComponent';
 import '@testing-library/jest-dom'
-import { MarkdownNode } from './MarkdownParser';
+import { MarkdownNode } from './md/types';
 import NodeHeaderComponent from './NodeHeaderComponent';
 
 describe('NodeHeaderComponent', () => {
     test('can render empty path', async () => {
-        const component = render(<NodeHeaderComponent path={[]} onPathClicked={() => {}}/>);
+        const component = render(<NodeHeaderComponent path={[]} onTitleClicked={() => {}}/>);
         const elts = await component.findAllByTestId('NodeHeaderComponent');
         expect(elts).toHaveLength(1);
     });
     test('can render many paths', async () => {
-        const component = render(<NodeHeaderComponent path={['hello', 'world']} onPathClicked={() => {}}/>);
+        const component = render(<NodeHeaderComponent path={['hello', 'world']} onTitleClicked={() => {}}/>);
         const helloItem = await component.getByText('hello');
         const worldItem = await component.getByText('world');
         expect(helloItem.innerHTML).toBe('hello');
         expect(worldItem.innerHTML).toBe('world');
     });
 
-    test('can render many paths', async () => {
-        let data = [];
-        const component = render(<NodeHeaderComponent path={['hello', 'world']} onPathClicked={(clickData) => { 
+    test('returns clicked titles', async () => {
+        let data: Array<string> = [];
+        const component = render(<NodeHeaderComponent path={['hello', 'world']} onTitleClicked={(clickData) => { 
             data.push(clickData);
          }}/>);
         const helloItem = await component.getByText('hello');
@@ -29,8 +29,8 @@ describe('NodeHeaderComponent', () => {
         fireEvent.click(helloItem);
         fireEvent.click(worldItem);
         expect(data).toStrictEqual([
-            ['hello'],
-            ['hello', 'world']
+            'hello',
+            'world'
         ]);
     });
 });
