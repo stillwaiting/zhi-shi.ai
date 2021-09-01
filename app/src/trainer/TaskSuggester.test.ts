fb86545956ccd7640cb04ffad10e383f00174a58
Boris Gvozdev <bgvozdev@gmail.com>
2021-09-01 18:58:14 +1000

    add task suggester class

---
+++ app/src/trainer/TaskSuggester.test.ts
@@ -0,0 +1,213 @@
+import TaskSuggester from './TaskSuggester';
+
+describe('TaskSuggester', () => {
+
+    let suggester!: TaskSuggester;
+
+    beforeEach(() => {
+        suggester = new TaskSuggester(`
+   
+# Root 1
+
+## SubNode 1
+
+### Rule 0: empty rule, no tasks
+
+### Rule 1: single tasks node
+
+#### Task  node 0
+
+Some text that should be ignored.
+
+? 0 Hello (foo|bar)
+! world
+
+? 1 Bar baz (hello|world)
+! hello
+
+### Rule 2: several task nodes
+
+#### Task node 1
+
+? 2 blah (a|b)
+! a
+
+? 3 blah (c|d)
+! c
+
+#### Task node 2
+
+? 4 blah (e|f)
+! e
+
+? 5 blah (g|h)
+! gs
+
+# Root 2
+
+## Rule: another root
+
+### Task node 3
+
+Some text that should be ignored.
+
+? 6 (Hello|bye)
+! hello
+
+? 7 (Cat|Dog)
+! cat
+
+        
+`)
+    });
+
+
+    test('On create builds tree of rules and tasks', () => {
+        const tree = suggester.getRulesTree();
+        expect(tree).toStrictEqual([
+            {
+              "title": "Root 1",
+              "children": [
+                {
+                  "title": "SubNode 1",
+                  "children": [
+                    {
+                      "title": "Rule: empty rule, no tasks",
+                      "rule": {
+                        "ruleIdx": 0,
+                        "nodeTitle": "Rule: empty rule, no tasks",
+                        "taskIdxs": [],
+                        "answeredTaskIdxs": [],
+                        "lastAnswers": []
+                      },
+                      "children": []
+                    },
+                    {
+                      "title": "Rule: single tasks node",
+                      "rule": {
+                        "ruleIdx": 1,
+                        "nodeTitle": "Rule: single tasks node",
+                        "taskIdxs": [
+                          0,
+                          1
+                        ],
+                        "answeredTaskIdxs": [],
+                        "lastAnswers": []
+                      },
+                      "children": []
+                    },
+                    {
+                      "title": "Rule: several task nodes",
+                      "rule": {
+                        "ruleIdx": 2,
+                        "nodeTitle": "Rule: several task nodes",
+                        "taskIdxs": [
+                          2,
+                          3,
+                          4,
+                          5
+                        ],
+                        "answeredTaskIdxs": [],
+                        "lastAnswers": []
+                      },
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "title": "Root 2",
+              "children": [
+                {
+                  "title": "Rule: another root",
+                  "rule": {
+                    "ruleIdx": 3,
+                    "nodeTitle": "Rule: another root",
+                    "taskIdxs": [
+                      6,
+                      7
+                    ],
+                    "answeredTaskIdxs": [],
+                    "lastAnswers": []
+                  },
+                  "children": []
+                }
+              ]
+            }
+          ]);
+    });
+
+    test('Selected rules limit the choice of tasks', () => {
+        suggester.setSelectedRuleIdxs([3]);
+        const suggestedTaskIdxs = new Set<number>();
+        for (let count = 0; count < 100; count ++ ) {
+            suggestedTaskIdxs.add(suggester.suggestNextTask().taskIdx);
+        }
+
+        const expected = new Set<number>([6,7]);
+        expected.add(6);
+        expected.add(7);
+        expect(suggestedTaskIdxs).toStrictEqual(expected);
+    });
+
+    test('Empty selected rules act as all rules', () => {
+        suggester.setSelectedRuleIdxs([]);
+
+        const suggestedTaskIdxs = new Set<number>();
+        for (let count = 0; count < 100; count ++ ) {
+            suggestedTaskIdxs.add(suggester.suggestNextTask().taskIdx);
+        }
+
+        const expected = new Set<number>([0,1,2,3,4,5,6,7]);
+        expect(suggestedTaskIdxs).toStrictEqual(expected);
+    });
+
+    test('Answer excludes the task from the pool of tasks to select', () => {
+        suggester.setSelectedRuleIdxs([2]);
+        const suggestedTaskIdxs = new Set<number>();
+        for (let count = 0; count < 100; count ++ ) {
+            if (suggestedTaskIdxs.size == 4) {
+                suggestedTaskIdxs.clear();
+            }
+            const nextTaskIdx = suggester.suggestNextTask().taskIdx;
+            suggester.recordAnswer(nextTaskIdx, true);
+            expect(suggestedTaskIdxs).not.toContain(nextTaskIdx);
+            suggestedTaskIdxs.add(nextTaskIdx);
+        }
+    });
+
+    test('The answered task is added back to the pool when all other tasks are answered, too', () => {
+        suggester.setSelectedRuleIdxs([2]);
+        const stats: {[taskIdx:number] : number} = {};
+        for (let count = 0; count < 80; count ++ ) {
+            const nextTaskIdx = suggester.suggestNextTask().taskIdx;
+            suggester.recordAnswer(nextTaskIdx, true);
+            stats[nextTaskIdx] = stats[nextTaskIdx] ? stats[nextTaskIdx] + 1 : 1;
+        }
+        expect(stats).toStrictEqual({2:20, 3:20, 4:20, 5:20});
+    });
+
+    test('Incorrect answer forces to answer tasks from the same rule', () => {
+        suggester.recordAnswer(2, false);
+        const suggestedTaskIdxs = new Set<number>();
+        for (let count = 0; count < 9; count ++ ) {
+            const nextTaskIdx = suggester.suggestNextTask().taskIdx;
+            suggester.recordAnswer(nextTaskIdx, true);
+            suggestedTaskIdxs.add(nextTaskIdx);
+        }
+        expect(suggestedTaskIdxs).toStrictEqual(new Set([2,3,5,4]));
+    });
+
+    test('When answered incorrectly and then correctly several times, the enforcement to answer only specific rule goes away', () => {
+        suggester.recordAnswer(2, false);
+        const suggestedTaskIdxs = new Set<number>();
+        for (let count = 0; count < 900; count ++ ) {
+            const nextTaskIdx = suggester.suggestNextTask().taskIdx;
+            suggester.recordAnswer(nextTaskIdx, true);
+            suggestedTaskIdxs.add(nextTaskIdx);
+        }
+        expect(suggestedTaskIdxs).toStrictEqual(new Set([0,1,2,3,5,4,6,7]));
+    });
+
+});
---
+++ app/src/trainer/TaskSuggester.ts
@@ -0,0 +1,157 @@
+import { timeStamp } from 'node:console';
+import mdParse from '../md/MarkdownParser';
+import { isMarkdownBodyChunkQuestionAnswers, MarkdownBodyChunkQuestionAnswers, MarkdownNode } from '../md/types';
+
+// Keep TaskType flat to avoid circular dependencies
+type TaskType = {
+    taskIdx: number;
+    bodyChunk: MarkdownBodyChunkQuestionAnswers;
+    ruleIdx: number;
+}
+
+// Keep RuleType flat to avoid circular dependencies
+type RuleType = {
+    ruleIdx: number;
+    nodeTitle: string;
+    taskIdxs: Array<number>;
+    answeredTaskIdxs: { [taskIdx: number]: boolean }; // purged when full; true - correctly, false - incorrectly
+    lastAnswers: Array<boolean>; // true - correctly, false - incorrectly
+}
+
+type RuleTreeLeafType = {
+    title: string,
+    rule: RuleType | undefined,
+
+    children: Array<RuleTreeLeafType> // always empty for "rule" nodes
+}
+
+export default class TaskSuggester {
+
+    // Each RuleType object has 2 references: one from the tree, another one from the
+    // array (with index = RuleType.ruleIdx)
+    // Tasks are not included in the tree, thus keeping it small and making it easy to operate (e.g. to clone)
+    private rulesTree: Array<RuleTreeLeafType> = [];
+    private rules: Array<RuleType> = [];
+
+    // Each TaskType has only a single reference
+    private tasks: Array<TaskType> = []; // taskIdx is the index in this array
+
+    private selectedRuleIdxs: Array<number> = []; // empty means all
+    private lastAnseredRuleIdx: number = -1;
+
+        constructor(rawMdData: string) {
+        const tree = mdParse(rawMdData, []);
+
+        tree.forEach(node => {
+            const leaf = this.parseMarkdown(node);
+            this.rulesTree.push(leaf);
+        });
+    }
+
+    setSelectedRuleIdxs(ruleIdxs: Array<number>) {
+        this.selectedRuleIdxs = JSON.parse(JSON.stringify(ruleIdxs));
+        if (ruleIdxs.indexOf(this.lastAnseredRuleIdx) < 0) {
+            this.lastAnseredRuleIdx = -1;
+            ruleIdxs.forEach(ruleIdx => {
+                const rule = this.rules[ruleIdx];
+                if (this.lastAnseredRuleIdx == -1 && this.hasIncorrectAnswer(rule)) {
+                    this.lastAnseredRuleIdx = ruleIdx;
+                }
+            })
+        }
+    }
+
+    getRulesTree(): Array<RuleTreeLeafType> {
+        return JSON.parse(JSON.stringify(this.rulesTree));
+    }
+
+    isTaskInSelectedRules(taskIdx: number): boolean {
+        if (this.selectedRuleIdxs.length == 0) {
+            return true;
+        }
+        return this.selectedRuleIdxs.indexOf(this.tasks[taskIdx].ruleIdx) >= 0;
+    }
+
+    recordAnswer(taskIdx: number, isCorrect: boolean) {
+        const rule = this.rules[this.tasks[taskIdx].ruleIdx];
+        if (Object.keys(rule.answeredTaskIdxs).length == rule.taskIdxs.length) {
+            rule.answeredTaskIdxs = { [taskIdx] : isCorrect };
+        } else {
+            rule.answeredTaskIdxs[taskIdx] = isCorrect;
+        }
+        rule.lastAnswers.push(isCorrect);
+        if (rule.lastAnswers.length > 10) {
+            rule.lastAnswers.shift();
+        }
+        this.lastAnseredRuleIdx = rule.ruleIdx;
+    }
+    
+    suggestNextTask(): TaskType {
+        const taskIdxsToLookIn: Array<number> = [];
+        if (this.lastAnseredRuleIdx >= 0 && this.hasIncorrectAnswer(this.rules[this.lastAnseredRuleIdx])) {
+            taskIdxsToLookIn.push(...this.getNotAnsweredTaskIdx(this.rules[this.lastAnseredRuleIdx]));
+        } else if (this.selectedRuleIdxs.length > 0) {
+            this.selectedRuleIdxs.forEach(ruleIdx => 
+                taskIdxsToLookIn.push(...this.getNotAnsweredTaskIdx(this.rules[ruleIdx])));
+
+        } else {
+            this.rules.forEach(rule => 
+                taskIdxsToLookIn.push(...this.getNotAnsweredTaskIdx(rule)));
+        }
+        return JSON.parse(JSON.stringify(
+            this.tasks[taskIdxsToLookIn[Math.floor(Math.random() * taskIdxsToLookIn.length)]]
+        ));
+    }
+
+    private getNotAnsweredTaskIdx(rule: RuleType): Array<number> {
+        if (Object.keys(rule.answeredTaskIdxs).length == rule.taskIdxs.length) {
+            return rule.taskIdxs;
+        }
+        return rule.taskIdxs.filter(taskIdx => rule.answeredTaskIdxs[taskIdx] === undefined);
+    }
+
+    private hasIncorrectAnswer(rule: RuleType): boolean {
+        return rule.lastAnswers.indexOf(false) >= 0;
+    }
+
+    private parseMarkdown(node: MarkdownNode): RuleTreeLeafType {
+        let leaf: RuleTreeLeafType = {
+            title: node.title,
+            rule: undefined,
+            children: []
+        };
+        if (node.title.indexOf("Rule") == 0) {
+            this.rules.push({
+                ruleIdx: this.rules.length,
+                nodeTitle: node.title,
+                taskIdxs: [],
+                answeredTaskIdxs: [],
+                lastAnswers: []
+            });
+            leaf.rule = this.rules[this.rules.length - 1];
+        }
+        if (node.title.indexOf("Task") == 0) {
+            node.body.content.forEach(maybeTask => {
+                if (isMarkdownBodyChunkQuestionAnswers(maybeTask)) {
+                    const task: TaskType = {
+                        taskIdx: this.tasks.length,
+                        bodyChunk: maybeTask,
+                        ruleIdx: this.rules.length - 1
+                    };
+                    this.tasks.push(task);
+                    this.rules[this.rules.length - 1].taskIdxs.push(this.tasks.length - 1);
+                }
+            })
+        }
+
+        
+        node.children.forEach(child => {
+            const childLeaf = this.parseMarkdown(child)
+            if (node.title.indexOf("Rule") != 0) {
+                leaf.children.push(childLeaf);
+            }
+        });
+
+        return leaf;
+    }
+}
