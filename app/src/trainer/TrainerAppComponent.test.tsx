a5b88ecd081d2cc820113c123b583484f5fd5a6a
Boris Gvozdev <bgvozdev@gmail.com>
2021-09-05 08:48:36 +1000

    extract selected ruleIdxs from path

--- app/src/trainer/TrainerAppComponent.test.tsx
+++ app/src/trainer/TrainerAppComponent.test.tsx
@@ -3,14 +3,47 @@ import { expect } from '@jest/globals';
 import { fireEvent, render, RenderResult, screen, cleanup, act } from '@testing-library/react';
 import fetchMock from 'jest-fetch-mock';
 import TrainerAppComponent from './TrainerAppComponent';
+import { MemoryRouter } from 'react-router';
+import { Link, Route } from "react-router-dom";
 
 function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function answerCorrectly(component: RenderResult) {
+    const firstSelect = component.container.getElementsByTagName('select')[0]
+    fireEvent.change(firstSelect, {
+        target: {value: 0}
+    });
+}
+
+
 describe('TrainerAppComponent', () => {
 
+    let currentPath: string = '/';
+
+    async function renderAndWaitForData(): Promise<RenderResult> {
+        const component = render(<MemoryRouter initialEntries={[currentPath]}> 
+            <TrainerAppComponent url='/foo' />
+    
+            <Route
+                path="*"
+                render={({ history, location }) => {
+                    currentPath = location.pathname;
+                    return null;
+                }}
+            />
+            <Link data-testid='goto1' to='/1'>goto 1</Link>
+            <Link data-testid='goto0' to='/0'>goto 0</Link>
+            
+            </MemoryRouter>)
+        await act(async () => { await sleep(1); });
+        return component;
+    }
+
+
     beforeEach(() => {
+        currentPath = '/'
         fetchMock.enableMocks();
         fetchMock.mockResponse(`
 
@@ -25,6 +58,10 @@ Some text that should be ignored.
 ? 6 (foo|bar)
 ! hello
 
+## Rule blah2
+
+### Task node 4
+
 ? 7 (hello|world)
 ! cat
 
@@ -37,19 +74,53 @@ Some text that should be ignored.
     });
 
     test('shows browser warning', async () => {
-        const component = render(<TrainerAppComponent url='/foo' />)
-        await act(async () => { await sleep(1); });
+        const component = await renderAndWaitForData();
         expect(component.getByTestId('BrowserWarning')).toBeDefined();
     });
 
 
     test('downloads data and then shows the task', async () => {
-        const component = render(<TrainerAppComponent url='/foo' />)
-        await act(async () => { await sleep(1); });
+        const component = await renderAndWaitForData();
+
         if (component.container.innerHTML.indexOf('hello') >= 0) {
             expect(component.getByText('hello')).toBeDefined();
         } else {
             expect(component.getByText('foo')).toBeDefined();
         }
+    });
+
+    test('gets selected rules from path', async() => {
+        currentPath = '/1';
+        
+        const component = await renderAndWaitForData();
+
+        for (let idx = 0; idx < 100; idx++) {
+            answerCorrectly(component);
+            fireEvent.click(component.getByText('Submit'));
+            fireEvent.click(component.getByText('Next'));
+            expect(component.getByText('world')).toBeDefined();
+        }
+    });
+
+    test('next records answer and opens another task', async () => {
+        const component = await renderAndWaitForData();
+
+        const hasWorld1 = component.container.innerHTML.indexOf('world') >= 0;
+        answerCorrectly(component);
+        fireEvent.click(component.getByText('Submit'));
+        fireEvent.click(component.getByText('Next'));
+        const hasWorld2 = component.container.innerHTML.indexOf('world') >= 0;
+        expect(new Set<boolean>([hasWorld1, hasWorld2])).toStrictEqual(new Set<boolean>([true, false]));
+    });
+
+    test('updates selected rule when path changes', async() => {
+        const component = await renderAndWaitForData();
+
+        const hasWorld1 = component.container.innerHTML.indexOf('world') >= 0;
+        
+        fireEvent.click(component.getByTestId(hasWorld1 ? 'goto0' : 'goto1'));
+        
+        const hasWorld2 = component.container.innerHTML.indexOf('world') >= 0;
+        expect(new Set<boolean>([hasWorld1, hasWorld2])).toStrictEqual(new Set<boolean>([true, false]));
     });
 });
--- app/src/trainer/TrainerAppComponent.tsx
+++ app/src/trainer/TrainerAppComponent.tsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import BodyQuestionAnswerComponent, { Context } from "../body/BodyQuestionAnswerComponent";
 import BrowserWarningComponent from "./BrowserWarningComponent";
 import DataProviderComponent from "./DataProviderComponent";
 import TaskSuggester, { TaskType } from "./TaskSuggester";
-
-let taskSuggester: TaskSuggester | undefined = undefined;
+import { BrowserRouter as Router, Route, Link, useHistory, useLocation } from "react-router-dom";
+import { extractSelectedRulIdxsFromPath } from "./pathutils";
 
 export default function({ url }: { url:string }) {
+    const location = useLocation();
     const [task, setCurrentTask] = useState<TaskType | null>(null);
     const [isAnswered, setIsAnswered] = useState<boolean>(false);
+    const [taskSuggester, setTaskSuggester] = useState<TaskSuggester | null>(null);
+    const [questionCounter, setQuestionCounter] = useState<number>(0);
+    const [selectedRuleIdxs, setSelectedRuleIdxs] = useState<Array<number>>(extractSelectedRulIdxsFromPath(location.pathname));
+
+    const history = useHistory();
+
+    useEffect(() => {
+        const newSelectedRules = extractSelectedRulIdxsFromPath(location.pathname);
+        setSelectedRuleIdxs(extractSelectedRulIdxsFromPath(location.pathname));
+        if (taskSuggester) {
+            taskSuggester.setSelectedRuleIdxs(newSelectedRules);
+            if (!taskSuggester.isTaskInSelectedRules(task!.taskIdx)) {
+                setCurrentTask(taskSuggester.suggestNextTask());
+            }
+        }
+    }, [location.pathname, task, taskSuggester, history]);
+    
     return <div className='TrainerComponent'>
             <BrowserWarningComponent />
             <DataProviderComponent url={process.env.PUBLIC_URL + url} onDataProvided={(data) => {
-                 taskSuggester = new TaskSuggester(data);
+                 const taskSuggester = new TaskSuggester(data);
+                 taskSuggester.setSelectedRuleIdxs(selectedRuleIdxs);
                  setCurrentTask(taskSuggester.suggestNextTask());
+                 setTaskSuggester(taskSuggester);
             }}
                  />
             {!!task 
-                ? <div><BodyQuestionAnswerComponent data = {task.bodyChunk} onAnswered={
+                ? <div><BodyQuestionAnswerComponent key={questionCounter} data = {task.bodyChunk} onAnswered={
                     (isCorrect) => {
                         taskSuggester!.recordAnswer(task.taskIdx, isCorrect);
                         setIsAnswered(true);
@@ -30,6 +50,7 @@ export default function({ url }: { url:string }) {
                 ? <button onClick={() => {
                     setCurrentTask(taskSuggester!.suggestNextTask());
                     setIsAnswered(false);
+                    setQuestionCounter(questionCounter + 1);
                 }}>Next</button>
                 : null
             }
